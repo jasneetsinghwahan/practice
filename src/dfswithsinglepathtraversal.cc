d83027b5d1c1d5847861df2117375d6414bed09f
jasneetsingh <jasneets@usc.edu>
2023-10-02 19:03:34 -0700

    topological sort added

--- src/dfswithsinglepathtraversal.cc
+++ src/dfswithsinglepathtraversal.cc
@@ -12,7 +12,7 @@ class MapSetGraph {
       : weights_(w), edge_map_(em) {}
 
   void DFS(int root, int target);
-  void DFS_iterative(int root, int target);
+  void DFS_iterative(int root, int target, int allPathstracked);
   void BFS(int root);
 
   // A helper function for DFS that carries marks in recursive calls
@@ -36,6 +36,29 @@ class MapSetGraph {
   std::map<std::pair<int, int>, int> weights_;
   std::map<int, std::set<int>> edge_map_;
 };
+
+std::vector<int> 
+MapSetGraph::TopologicalSort(int root){
+  std::map<int, int> marks;
+  std::map<int, int> parent;
+  std::vector<int> topo_list;
+  DFS_helper_with_topo(root, marks, topo_list);
+  return topo_list;
+}
+
+void MapSetGraph::DFS_helper_with_topo(int root, std::map<int, int> &marks,
+                            std::vector<int> &topo_list){
+    
+    //if(marks[root] == 1)    // root is visited
+    //    return;
+    marks[root] = 1;
+    for (int child : edge_map_[root]){
+        if (marks[child] != 1){
+            DFS_helper_with_topo(child, marks, topo_list);
+        }
+    }
+    topo_list.push_back(root);
+}
 
 void MapSetGraph::DFS(int root, int target) {
   std::map<int, int> marks;
@@ -72,7 +95,7 @@ void MapSetGraph::DFS_helper(int root, int target, std::map<int, int> &marks, st
   // At this point, all children of root are visited.
 }
 
-void MapSetGraph::DFS_iterative(int root, int target){
+void MapSetGraph::DFS_iterative(int root, int target, int allPathstracked = false){
     if (root == target){
         std::cout << "target same as src" << std::endl;
         return;
@@ -86,22 +109,45 @@ void MapSetGraph::DFS_iterative(int root, int target){
         stk.pop();
         marks[currnode] = 1;    // mark as visited
         for (int child: edge_map_[currnode]){
-            if (marks[child] != 1){
+            // parent map searched to endure that node whose parent is marked is not overwritten
+            if (marks[child] != 1 && (parent.find(child) == parent.end())){
                 parent[child] =  currnode;
-                if (child == target)
+                if (child == target && allPathstracked == false)
                     break;
                 stk.push(child);
             }
         }
     }
-    printRoute(root, target, parent);
+    if (allPathstracked == false)
+        printRoute(root, target, parent);
+    else {
+        auto itr = edge_map_.begin();
+        for (; itr != edge_map_.end(); itr++){
+            if (itr->first != root){
+                printRoute(root, itr->first, parent);
+            }
+        }
+    }
 }
 
 int main()
 {
-    std::map<int, std::set<int>> edges = {{0, {1,2}},{1, {0,3}}, {2, {0,3}}, {3, {1,2,4,5}}, {4, {3}}, {5, {3}}};
+    std::map<int, std::set<int>> edges = {{0, {1,2, 5}},{1, {0,2, 3}}, {2, {0,1, 5, 3}}, {3, {1,2, 4, 6}}, {4, {3}}, {5, {0, 2}}, {6, {3}}};
     MapSetGraph g(edges);
+    std::cout << "calling DFS recursive with path for single target" << std::endl;
     g.DFS(0, 4);
-    g.DFS_iterative(0, 4);
+    bool allPathstracked = false;
+    std::cout << "calling DFS iterative with path for single target" << std::endl;
+    g.DFS_iterative(0, 1, allPathstracked);
+    std::cout << "calling DFS iterative with path for all target" << std::endl;
+    allPathstracked = true;
+    g.DFS_iterative(0, 4, allPathstracked);
+    std::map<int, std::set<int>> diredges = {{0, {1,2, 5}},{1, {2, 3}}, {2, {3}}, {3, {4, 6}}, {4, {}}, {5, {2}}, {6, {}}};
+    MapSetGraph h(diredges);
+    std::cout << "calling DFS with topo" << std::endl;
+    std::vector<int> topo_list = h.TopologicalSort(0);
+    for (int i = topo_list.size()-1; i >= 0; i--){
+        std::cout << "now at: " << topo_list[i] << '\n'; 
+    }
     return 0;
 }
